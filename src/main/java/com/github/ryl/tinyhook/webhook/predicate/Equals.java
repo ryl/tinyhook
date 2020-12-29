509a15ff26a3de069e0ce3db6be6152b91b42767
Ry Lowry <rylowry@gmail.com>
2020-12-29 01:06:53 -0600

    More output

--- src/main/java/com/github/ryl/tinyhook/webhook/predicate/Equals.java
+++ src/main/java/com/github/ryl/tinyhook/webhook/predicate/Equals.java
@@ -2,12 +2,16 @@ package com.github.ryl.tinyhook.webhook.predicate;
 
 import com.jayway.jsonpath.JsonPath;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.function.Predicate;
 
 
 @AllArgsConstructor
 public class Equals implements Predicate<String> {
+
+  private static final Logger logger = LoggerFactory.getLogger(Equals.class);
   
   private final String expression;
   
@@ -15,7 +19,9 @@ public class Equals implements Predicate<String> {
 
   @Override
   public boolean test(String s) {
-    return JsonPath.read(s, expression).equals(value);
+    boolean result = JsonPath.read(s, expression).equals(value);
+    logger.info("{} eq {} -> {}", expression, value, result);
+    return result;
   }
   
 }
--- src/main/java/com/github/ryl/tinyhook/webhook/predicate/Exists.java
+++ src/main/java/com/github/ryl/tinyhook/webhook/predicate/Exists.java
@@ -2,6 +2,8 @@ package com.github.ryl.tinyhook.webhook.predicate;
 
 import com.jayway.jsonpath.JsonPath;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 import java.util.function.Predicate;
@@ -9,11 +11,15 @@ import java.util.function.Predicate;
 @AllArgsConstructor
 public class Exists implements Predicate<String> {
 
+  private static final Logger logger = LoggerFactory.getLogger(Exists.class);
+
   private final String expression;
 
   @Override
   public boolean test(String s) {
-    return Objects.nonNull(JsonPath.read(s, expression));
+    var result = Objects.nonNull(JsonPath.read(s, expression));
+    logger.info("{} exists -> {}", expression, result);
+    return result;
   }
   
 }
